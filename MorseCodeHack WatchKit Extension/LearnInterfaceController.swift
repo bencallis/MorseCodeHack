84e11fe6fd6ff5a51d3c1d8d4b429bf5bf3872ae
Ben Callis <ben.callis@skyscanner.net>
2015-09-10 21:51:48 +0100

    Swiftly

---
+++ MorseCodeHack WatchKit Extension/LearnInterfaceController.swift
@@ -0,0 +1,46 @@
+//
+//  LearnInterfaceController.swift
+//  MorseCodeHack
+//
+//  Created by Ben Callis on 10/09/2015.
+//  Copyright © 2015 iOSDevUK. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class LearnInterfaceController: WKInterfaceController {
+
+    @IBOutlet var learnTable: WKInterfaceTable!
+    
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        let numberOfLetters = alphaNumToMorse.count
+        learnTable.setNumberOfRows(numberOfLetters, withRowType: "learnRow")
+        let sortedKeysAndValues = alphaNumToMorse.sort { $0.0 < $1.0 }
+
+        var counter = 0
+        for (character, code) in sortedKeysAndValues {
+            if let row = learnTable.rowControllerAtIndex(counter) as? LearnRow {
+                let stringRep = code.reduce("") { (codes, code) in codes + code.rawValue }
+                row.titleLabel.setText(character)
+                row.morseCodeLabel.setText(stringRep)
+                
+                counter++
+            }
+        }
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ MorseCodeHack WatchKit Extension/LearnRow.swift
@@ -0,0 +1,16 @@
+//
+//  LearnRow.swift
+//  MorseCodeHack
+//
+//  Created by Ben Callis on 10/09/2015.
+//  Copyright © 2015 iOSDevUK. All rights reserved.
+//
+
+import WatchKit
+
+class LearnRow: NSObject {
+    @IBOutlet weak var titleLabel: WKInterfaceLabel!
+    @IBOutlet weak var morseCodeLabel: WKInterfaceLabel!
+
+
+}
