834d136dacb54043d3a24dd216151b086f458ec4
Ben Callis <ben.callis@skyscanner.net>
2015-09-10 19:56:27 +0100

    Changed some colours of UI in table

--- MorseCodeHack WatchKit Extension/GameInterfaceController.swift
+++ MorseCodeHack WatchKit Extension/GameInterfaceController.swift
@@ -11,13 +11,19 @@ import Foundation
 
 
 class GameInterfaceController: WKInterfaceController {
+    
+    var score : Int = 0{
+        didSet {
+            currentScoreLabel.setText(String(score))
+        }
+    }
 
     @IBOutlet var currentScoreLabel: WKInterfaceLabel!
     
     @IBOutlet var choicesTable: WKInterfaceTable!
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
-        
+        reloadRound()
         // Configure interface objects here.
     }
 
@@ -26,23 +32,51 @@ class GameInterfaceController: WKInterfaceController {
         super.willActivate()
     }
 
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
+
+    func reloadRound() {
+        let newRound = getNextRound()
+        
+        let numberOfOptions = newRound.alternatives.count + 1
+        choicesTable.setNumberOfRows(numberOfOptions, withRowType: "choiceRow")
+
+        var arrayOfChoices = newRound.alternatives + [newRound.letter]
+        for index in 0..<arrayOfChoices.count {
+            let swapIndex = Int(arc4random_uniform(UInt32(numberOfOptions)))
+            let temp = arrayOfChoices[index]
+            arrayOfChoices[index] = arrayOfChoices[swapIndex]
+            arrayOfChoices[swapIndex] = temp
+        }
+        
+        for index in 0..<4 {
+            if let row = choicesTable.rowControllerAtIndex(index) as? SimpleRow {
+                row.titleLabel.setText(arrayOfChoices[index])
+            }
+        }
     }
     
-//    func reloadCurrentGame {
-//        // 1
-//        choicesTable.setNumberOfRows(10, withRowType: "CoinRow")
-//        
-//        for (index, coin) in enumerate(coins) {
-//            // 2
-//            if let row = coinTable.rowControllerAtIndex(index) as? CoinRow {
-//                // 3
-//                row.titleLabel.setText(coin.name)
-//                row.detailLabel.setText("\(coin.price)")
-//            }
-//        }
-//    }
+    override func table(table: WKInterfaceTable, didSelectRowAtIndex rowIndex: Int) {
+        if let row = table.rowControllerAtIndex(rowIndex) as? SimpleRow {
+            let isCorrect = true
+            let colour = isCorrect ? UIColor.redColor() : UIColor.greenColor()
+            row.group.setBackgroundColor(colour)
+            
+            if isCorrect {
+                handleCorrectAnswer()
+            } else {
+                handleWrongAnswer()
+            }
+        
+        }
+    }
+    
+    func handleCorrectAnswer () {
+        score++
+        // start new game
+        reloadRound()
+    }
+    
+    func handleWrongAnswer () {
+        // throw back to main menu
+    }
 
 }
